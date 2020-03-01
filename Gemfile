9d8f150af029e223a81e38f276c39a11a8b3f344
Ben S <bendev@Bens-MacBook-Air.local>
2020-03-01 01:39:25 -0500

    ruby add

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'devise'
 gem 'cancancan'
 gem 'draper'
 gem 'pundit'
+gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'home/index'
-  get 'home/about'
-  get 'home/contact'
-  get 'home/shop'
-
- root 'home#index'
+  get  '/about',   to: 'home#about'
+  get  '/contact', to: 'home#contact'
+  get  '/shop',    to: 'home#shop'
+  
+  root 'home#index'
 
 end
